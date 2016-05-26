f766ffa2c62eea13b7f1b546a6ea28cb361a6fa1
socrets <fullstack_dev@126.com>
2016-05-26 21:23:09 +0800

    rot13.js added

---
+++ basic-algorithm/rot13.js
@@ -0,0 +1,18 @@
+function rot13(str) {
+    var re = /[A-Z]/i;
+    var strRotArr = [];
+
+    for(var i = 0; i < str.length; i++) {
+	if (re.test(str[i])) {
+	    if (str.charCodeAt(i) > 77) {
+		strRotArr.push(String.fromCharCode(str.charCodeAt(i) - 13));		            }
+	    else {
+		strRotArr.push(String.fromCharCode(str.charCodeAt(i) + 13));		            }	
+	}
+	else {
+	    strRotArr.push(str[i]);
+	}
+    }
+
+    return strRotArr.join("");
+}
